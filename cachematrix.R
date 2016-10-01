144543eceb742cd533b3de164e40be06b8a73d9d
DavidVD <david.vd@outlook.com>
2016-10-01 22:31:52 +0200

    Solved assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,54 @@
-makeVector <- function(x = numeric()) {
-        m <- NULL
+## This is the assignment for 3rd week of R programming on Coursera
+## In this we have to create two functions over a matrix to get the inverse
+## of that and, if it is done previously over that matrix, get the cached 
+## inverse instead of compute it again.
+
+
+## makeCacheMatrix does the following actions: - set & get the value of a
+## matrix, and set & get the inverse of it.
+
+makeCacheMatrix <- function(x = matrix()) {
+        
+        i <- NULL
+        # Set
         set <- function(y) {
                 x <<- y
-                m <<- NULL
+                i <<- NULL
         }
+        
+        # Get
         get <- function() x
-        setmean <- function(mean) m <<- mean
-        getmean <- function() m
-        list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
+        
+        # Set inverse
+        setinverse <- function(inverse) i <<- inverse
+        
+        # Get inverse
+        getinverse <- function() i
+        
+        list(set=set, 
+             get=get, 
+             setinverse=setinverse, 
+             getinverse=getinverse)
 }
 
+
+## Write a short comment describing this function
+
+## CacheSolve retrieves the inverse of a matrix assuming that it is
+## allways invertible.
+
+cacheSolve <- function(x, ...) {
+        
+        i <- x$getinverse()
+        
+        if(!is.null(i)) {
+                return(i)
+        }
+        
+        data <- x$get()
+        i <- solve(data)
+        x$setinverse(i)
+        
+        print(i)
+        
+}
